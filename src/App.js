4b3626f643819407e0de5c000247c23c4625c267
deremije <jeremy@deremije.com>
2020-06-05 14:36:29 +0200

    Added photo support

--- src/App.js
+++ src/App.js
@@ -5,36 +5,36 @@ import TableComponent from "./TableComponent/TableComponent"
 
 class App extends React.Component {
   state = {
-    headers: ['email', 'name', 'gender'],
-    data: [
-        {
-            email: 'jeremydamon@gmail.com',
-            name: 'Jeremy Damon',
-            gender: 'male'
-        },
-        {
-            email: 'jeremyrandall1980@gmail.com',
-            name: 'Jeremy Randall',
-            gender: 'male'
-        },
-        {
-            email: 'shannou1@gmail.com',
-            name: 'Shannon Randall',
-            gender: 'female'
-        },
-        {
-            email: 'zoetsarandall@gmail.com',
-            name: 'Zoetsa Randall',
-            gender: 'female'
-        },
-        
-    ]
+    headers: ['name', 'photo', 'email', 'cell', 'gender', 'nationality'],
+    data: null
   }
   
+  componentDidMount() {
+    fetch("https://randomuser.me/api/?results=500")
+      .then(response => response.json())
+      .then(response => response.results.map(entry => {
+        return {
+          photo: entry.picture.thumbnail,
+          name: `${entry.name.last}, ${entry.name.first}`,
+          email: entry.email,
+          cell: entry.cell,
+          gender: entry.gender,
+          nationality: entry.nat,
+          id: entry.login.uuid
+        }
+      }))
+      .then(results => this.setState({ data: results }))
+      .catch(error => console.log(error))
+  }
+
+
   render() {
     return (
       <div className="App">
-        <TableComponent headers={this.state.headers} data={this.state.data} />
+        {this.state.data ? 
+          <TableComponent headers={this.state.headers} data={this.state.data} /> :
+          <div>Loading Table...</div>
+        }
       </div>
     )
   }
--- src/TableComponent/TableComponent.js
+++ src/TableComponent/TableComponent.js
@@ -1,42 +1,86 @@
 import React from 'react'
 import { Suspense, lazy } from 'react';
+import { Resizable } from 're-resizable';
+
 
 import './TableComponent.scss'
 
 const DataItem = lazy(() => import("./DataItem/DataItem"))
 
 class TableComponent extends React.Component {
-    render() {
+    state = {
+        ascending: true,
+        sortBy: ""
+    }
+
+    componentDidMount() {
+        if (this.props.headers) this.setState({sortBy: this.props.headers[0]})
+    }
+
+    columnSort(header) {
+        if (this.state.sortBy === header) {
+            this.setState({ascending: !this.state.ascending})
+        } else {
+            this.setState({sortBy: header, ascending: true})
+        }
+    }
+
+    caretStyle(header) {
+        let style = {
+            opacity: '0',
+            transform: 'rotate(45deg)'
+        }
         
+        if (header === this.state.sortBy) style.opacity = '1'
+        if (!this.state.ascending) style.transform = 'rotate(-135deg)'
+
+        return style
+    }
+    
+    sortData() {
+        let array = [...this.props.data]
+        array.sort((a,b) => {
+            if (a[this.state.sortBy] > b[this.state.sortBy]) return this.state.ascending ? 1 : -1
+            if (a[this.state.sortBy] < b[this.state.sortBy]) return this.state.ascending ? -1 : 1
+            return 0
+        })
+        return array
+    }
+    
+    render() {
+        const sortedData = this.sortData()
+
         return (
             <div className='tableComponent'>
+                
                 {this.props.headers.map(header => 
-                    <div className='column'>
-                        <div className='header'>
-                            {header}
-                        </div>
-                        {this.props.data.map(line => 
-                            <Suspense fallback={<div className='tableRow'>Loading...</div>}>
-                                <DataItem line={line} header={header} />
-                            </Suspense>
-                        )}
-                    </div>
+                    <Resizable 
+                        key={header}
+                        enable={{ 
+                            top:false, 
+                            right:true, 
+                            bottom:false, 
+                            left:false, 
+                            topRight:false, 
+                            bottomRight:false, 
+                            bottomLeft:false, 
+                            topLeft:false 
+                        }} 
+                        className='column'>
+                            <div className='header' onClick={() => this.columnSort(header)}>
+                                {header} <span className='caret' style={this.caretStyle(header)} />
+                            </div>
+                            {sortedData.map(line => 
+                                <Suspense key={line.id} fallback={<div className='dataItem'>Loading {header}...</div>}>
+                                    {header === "photo" ?
+                                        <div className='imgWrapper'><img src={line.photo} alt={line.name} /></div> :
+                                        <DataItem line={line} header={header} />}
+                                </Suspense>
+                            )}
+                    </Resizable>
                 )}
             </div>
         )
-
-        // return (
-        //     <div className='tableComponent'>
-        //         <div className='headerRow'>
-        //             {this.state.headers.map(header => <div className='headerItem' key={header}>{header}</div>)}
-        //         </div>
-        //         {this.state.data.map(line => 
-        //             <Suspense fallback={<div className='tableRow'>Loading...</div>}>
-        //                 <DataLine line={line} headers={this.state.headers} />
-        //             </Suspense>
-        //         )}
-        //     </div>
-        // )
     }
 }
 
